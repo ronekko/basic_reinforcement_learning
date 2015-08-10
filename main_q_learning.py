37aa1cd3a9bc56d3138844fb82168fc6fcd20dae
ronekko <ronekko@gmail.com>
2015-08-10 14:09:37 +0900

    Add Agent class

--- main_q_learning.py
+++ main_q_learning.py
@@ -10,96 +10,125 @@ import numpy as np
 import matplotlib.pyplot as plt
 import vrep
 import contexttimer
-
-# define actions
-num_actions_0 = 3
-num_actions_1 = 3
-num_actions = num_actions_0 * num_actions_1
-angles_0 = np.linspace(0, np.pi/2, num_actions_0)
-angles_1 = np.linspace(0, np.pi/2, num_actions_0)
-angles_lut = np.array(np.meshgrid(angles_1, angles_0,
-                                  indexing='ij')).reshape(2, -1).T
-
-# define states
-# state = 0 if x < pi/2K, 1 if pi/2K <= x < 2pi/2K, ..., K-1 if (K-1)pi/2K <= x
-num_states_0 = 5  # angle of joint 0
-num_states_1 = 5  # angle of joint 1
-num_states = num_states_0 * num_states_1
-state_bins = [np.linspace(0, np.pi/2, num_states_0, endpoint=False)[1:],
-              np.linspace(0, np.pi/2, num_states_1, endpoint=False)[1:]]
-
-# define Q-table
-q_table = np.zeros((num_states, num_actions), dtype=np.float) + 0.5
+from environment import Robot
 
 
-def choose_action(state):
-    epsilon = 0.01
-    if np.random.uniform() < epsilon:
-        action = np.random.choice(num_actions)
-    else:
-        action = np.argmax(q_table[state])
-    return action
+class Agent(object):
+    def __init__(self, robot, alpha=0.1, gamma=0.9, epsilon=0.05, q_init=1):
+        self.robot = robot
+        self.num_actions_0 = 3
+        self.num_actions_1 = 3
+        self.num_actions = self.num_actions_0 * self.num_actions_1
+        self.angles_0 = np.linspace(0, np.pi/2, self.num_actions_0)
+        self.angles_1 = np.linspace(0, np.pi/2, self.num_actions_1)
+        # look-up table from action to angles
+        self.angles_lut = np.array(np.meshgrid(self.angles_1, self.angles_0,
+                                   indexing='ij')).reshape(2, -1).T
+
+        self.num_states_0 = 5  # angle of joint 0
+        self.num_states_1 = 5  # angle of joint 1
+        self.num_states = self.num_states_0 * self.num_states_1
+        self.state_bins = [
+            np.linspace(0, np.pi/2, self.num_states_0, endpoint=False)[1:],
+            np.linspace(0, np.pi/2, self.num_states_1, endpoint=False)[1:]]
+
+        self.q_table = np.full((self.num_states, self.num_actions), q_init)
+        self.alpha = alpha      # learning rate
+        self.gamma = gamma      # discount factor
+        self.epsilon = epsilon  # epsilon-greedy rate
+
+    def choose_action(self, state):
+        if np.random.uniform() < self.epsilon:
+            action = np.random.choice(self.num_actions)
+        else:
+            action = np.argmax(self.q_table[state])
+        return action
+
+    def do_action(self, action):
+        angles = self.angles_lut[action]
+        self.robot.set_joint_angles(angles)
+        self.robot.proceed_simulation()
+
+    def observe_state(self):
+        angles = self.robot.get_joint_angles()
+        return self.calc_state(angles)
+
+    def calc_state(self, angles):
+        state_0 = np.digitize([angles[0]], self.state_bins[0])[0]
+        state_1 = np.digitize([angles[1]], self.state_bins[1])[0]
+        state = state_0 * self.num_states_1 + state_1
+        return state
+
+    def play(self):
+        action = self.choose_action(self.state)
+        self.do_action(action)
+
+        state_new = self.observe_state()
+
+        position_new = self.robot.get_body_position()
+        x_forward = position_new[0] - self.position[0]
+        reward = x_forward - 0.001
+
+        # update Q-table
+        self.update_q(self.state, action, reward, state_new)
+
+        self.state = state_new
+        self.position = position_new
+
+    def update_q(self, state, action, reward, state_new):
+        q_sa = self.q_table[state, action]
+        td_error = reward + self.gamma * np.max(self.q_table[state_new]) - q_sa
+        self.q_table[state, action] = q_sa + self.alpha * td_error
+
+    def initialize_episode(self):
+        self.robot.restart_simulation()
+        self.robot.initialize_pose()
+        self.position = self.robot.get_body_position()
+        angles = self.robot.get_joint_angles()
+        self.state = self.calc_state(angles)
 
 
-def calc_state(angles):
-    state_0 = np.digitize([angles[0]], state_bins[0])[0]
-    state_1 = np.digitize([angles[1]], state_bins[1])[0]
-    state_new = state_0 * num_states_1 + state_1
-    return state_new
+def plot(body_trajectory, joints_trajectory, return_history, q_table):
+    fig = plt.figure(figsize=(9, 4))
+    T = len(body_trajectory)
 
+    # plot an xyz trajectory of the body
+    ax1 = plt.subplot(221)
+    ax2 = plt.subplot(223)
+    ax3 = plt.subplot(222)
+    ax4 = plt.subplot(224)
+    ax1.grid()
+    ax1.set_color_cycle('rgb')
+    ax1.plot(np.arange(T) * 0.05, np.array(body_trajectory))
+    ax1.set_title('Position of the body')
+    ax1.set_ylabel('position [m]')
+    ax1.legend(['x', 'y', 'z'], loc='best')
 
-def act(joints, state, body_pos):
-    alpha = 0.1  # learning rate
-    gamma = 0.9  # discount factor
+    # plot a trajectory of angles of the joints
+    ax2.grid()
+    ax2.set_color_cycle('rg')
+    ax2.plot(np.arange(T) * 0.05, np.array(joints_trajectory))
+    ax2.set_title('Angle of the joints')
+    ax2.set_xlabel('time in simulation [s]')
+    ax2.set_ylabel('angle [rad]')
+    ax2.legend(['joint_0', 'joint_1'], loc='best')
 
-    action = choose_action(state)
-    angles = angles_lut[action]  # look-up table from action to angles
+    # plot a history of returns of each episode
+    ax3.grid()
+    ax3.plot(return_history)
+    ax3.set_title('Returns (total rewards) of each episode')
+    ax3.set_xlabel('episode')
+    ax3.set_ylabel('position [m]')
 
-    for joint, angle in zip(joints, angles):
-        if not np.isnan(angle):
-            e = vrep.simxSetJointTargetPosition(client_id, joint, angle,
-                                                vrep.simx_opmode_streaming)
-    vrep.simxSynchronousTrigger(client_id)
-
-    angles_new = []
-    for joint in joints:
-        e, angle_new = vrep.simxGetJointPosition(client_id, joint,
-                                                 vrep.simx_opmode_buffer)
-        angles_new.append(angle_new)
-
-    e, body_pos_new = vrep.simxGetObjectPosition(
-        client_id, body, -1, vrep.simx_opmode_buffer)
-    x_forward = body_pos_new[0] - body_pos[0]
-    reward = x_forward - 0.001
-    body_pos = body_pos_new
-
-    state_new = calc_state(angles_new)
-
-    q_sa = q_table[state, action]
-    td_error = reward + gamma * np.max(q_table[state_new]) - q_sa
-    q_table[state, action] = q_sa + alpha * td_error
-
-    return (state_new, body_pos_new)
-
-
-def restart_simulation(client_id, joints):
-    e = vrep.simxStopSimulation(client_id, vrep.simx_opmode_oneshot_wait)
-    time.sleep(0.5)
-    e = vrep.simxStartSimulation(client_id, vrep.simx_opmode_oneshot_wait)
-    e = vrep.simxSetJointTargetPosition(client_id, joints[0], 0,
-                                        vrep.simx_opmode_streaming)
-    e = vrep.simxSetJointTargetPosition(client_id, joints[1], 0,
-                                        vrep.simx_opmode_streaming)
-    for t in range(10):
-        vrep.simxSynchronousTrigger(client_id)
-    e, body_pos = vrep.simxGetObjectPosition(client_id, body, -1,
-                                             vrep.simx_opmode_streaming)
-    e, angle_0 = vrep.simxGetJointPosition(client_id, joints[0],
-                                           vrep.simx_opmode_streaming)
-    e, angle_1 = vrep.simxGetJointPosition(client_id, joints[1],
-                                           vrep.simx_opmode_streaming)
-    state = calc_state([angle_0, angle_1])
-    return (state, body_pos)
+    # show Q-table
+    ax4.matshow(q_table.T, cmap=plt.cm.gray)
+    ax4.set_title('Q-table')
+    ax4.xaxis.set_ticks_position('bottom')
+    ax4.set_xlabel('state')
+    ax4.set_ylabel('action')
+    plt.tight_layout()
+    plt.show()
+    plt.draw()
 
 
 if __name__ == '__main__':
@@ -110,98 +139,54 @@ if __name__ == '__main__':
     e = vrep.simxStopSimulation(client_id, vrep.simx_opmode_oneshot_wait)
     vrep.simxFinish(-1)
     client_id = vrep.simxStart('127.0.0.1', 19998, True, True, 5000, 5)
-
     assert client_id != -1, 'Failed connecting to remote API server'
-
-    e = vrep.simxStartSimulation(client_id, vrep.simx_opmode_oneshot_wait)
 
     # print ping time
     sec, msec = vrep.simxGetPingTime(client_id)
     print "Ping time: %f" % (sec + msec / 1000.0)
 
-    # Handles of body and wheels
-    e, body = vrep.simxGetObjectHandle(client_id, 'body',
-                                       vrep.simx_opmode_oneshot_wait)
-    e, joint_0 = vrep.simxGetObjectHandle(client_id, 'joint_0',
-                                          vrep.simx_opmode_oneshot_wait)
-    e, joint_1 = vrep.simxGetObjectHandle(client_id, 'joint_1',
-                                          vrep.simx_opmode_oneshot_wait)
-    joints = [joint_0, joint_1]
+    robot = Robot(client_id)
+    agent = Agent(robot, alpha=0.1, gamma=0.9, epsilon=0.05, q_init=0.3)
 
-    # Set the joint angles to the default position (0, 0)
-    e = vrep.simxSetJointTargetPosition(client_id, joints[0], 0,
-                                        vrep.simx_opmode_streaming)
-    e = vrep.simxSetJointTargetPosition(client_id, joints[1], 0,
-                                        vrep.simx_opmode_streaming)
-    state = 0
-
-    # this line is necessary to enable position recording
-    e, body_pos = vrep.simxGetObjectPosition(client_id, body, -1,
-                                             vrep.simx_opmode_streaming)
-    # Wait until the robot is settled to the default position
-    time.sleep(0.3)
-
-    # enable synchronous mode
-    vrep.simxSynchronous(client_id, True)
-
-    position_history = []
-    e, body_pos = vrep.simxGetObjectPosition(client_id, body, -1,
-                                             vrep.simx_opmode_buffer)
-    position_history.append(body_pos)
-
-    joint_pos_history = []
-    e, joint_0_pos = vrep.simxGetJointPosition(client_id, joints[0],
-                                               vrep.simx_opmode_streaming)
-    e, joint_1_pos = vrep.simxGetJointPosition(client_id, joints[1],
-                                               vrep.simx_opmode_streaming)
-    joint_pos_history.append([joint_0_pos, joint_1_pos])
-
-    num_episodes = 400
+    num_episodes = 500
     len_episode = 100
+    return_history = []
     try:
         for episode in range(num_episodes):
             print "start simulation # %d" % episode
+
             with contexttimer.Timer() as timer:
-                state, body_pos = restart_simulation(client_id, joints)
+                agent.initialize_episode()
+                body_trajectory = []
+                joints_trajectory = []
+                body_trajectory.append(robot.get_body_position())
+                joints_trajectory.append(robot.get_joint_angles())
 
                 for t in range(len_episode):
-                    state, body_pos = act(joints, state, body_pos)
-                    print state,
+                    agent.play()
+                    print agent.state,
 
-                    position_history.append(body_pos)
-                    e, joint_0_pos = vrep.simxGetJointPosition(
-                        client_id, joints[0], vrep.simx_opmode_buffer)
-                    e, joint_1_pos = vrep.simxGetJointPosition(
-                        client_id, joints[1], vrep.simx_opmode_buffer)
-                    joint_pos_history.append([joint_0_pos, joint_1_pos])
-                print
-            print "Body position: ", body_pos
+                    body_trajectory.append(robot.get_body_position())
+                    joints_trajectory.append(robot.get_joint_angles())
+
+            position = body_trajectory[-1]
+            return_history.append(position[0])
+            q_table = agent.q_table
+
+            plot(body_trajectory, joints_trajectory, return_history, q_table)
+            print
+            print "Body position: ", position
             print "Elapsed time (wall-clock): ", timer.elapsed
             print
+
     except KeyboardInterrupt:
         print "Terminated by `Ctrl+c` !!!!!!!!!!"
 
-    # plot xyz trajectory of the body
-    T = len(position_history)
-    plt.gca().set_color_cycle('rgb')
-    plt.plot(np.arange(T) * 0.05, np.array(position_history))
-    plt.legend(['x', 'y', 'z'], loc='best')
-    plt.title('Position of the body')
-    plt.xlabel('time in simulation [s]')
+    plt.grid()
+    plt.plot(return_history)
+    plt.title('Return (total reward in a episode)')
+    plt.xlabel('episode')
     plt.ylabel('position [m]')
-    plt.grid()
-    plt.show()
-
-    # plot joint positions
-    plt.figure()
-    T = len(joint_pos_history)
-    plt.gca().set_color_cycle('rgb')
-    plt.plot(np.arange(T) * 0.05, np.array(joint_pos_history))
-    plt.legend(['joint_0', 'joint_1'], loc='best')
-    plt.title('Position of the joints')
-    plt.xlabel('time in simulation [s]')
-    plt.ylabel('position [rad]')
-    plt.grid()
     plt.show()
 
     e = vrep.simxStopSimulation(client_id, vrep.simx_opmode_oneshot_wait)
